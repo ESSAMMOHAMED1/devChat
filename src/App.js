9a9bc613ad8775e5dcf42c3bef7f071d4fa3c5ce
Essam_Mohamed <99250205+ESSAMMOHAMED1@users.noreply.github.com>
2024-01-21 01:37:21 -0800

    Login-Register

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Register from "./pages/Register";
-
+import '../src/style.scss'
+import Login from "./pages/Login";
 function App() {
   return (
     <div className="App">
-     <Register/>
-    </div>
+     <Register />
+    </div>  
   );
 }
 
---
+++ src/pages/Login.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Login = () => {
+
+    return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">DevChat</span>
+        <span className="title">Login
+    </span>
+        <form>
+          <input required type="email" placeholder="email" />
+          <input required type="password" placeholder="password" />        
+          <button>Sign in</button>
+        </form>
+        <p>
+          You don't have an account? Register
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
