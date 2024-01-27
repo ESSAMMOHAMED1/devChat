09a912c5189107dccc97b4a183ed135584a9f28c
Essam_Mohamed <99250205+ESSAMMOHAMED1@users.noreply.github.com>
2024-01-27 00:46:58 -0800

    user-firebase-login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useContext } from "react";
 import {
   BrowserRouter,
@@ -14,13 +14,38 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import "../src/style.scss";
 import { AuthContext } from "./context/AuthContext";
+import { auth } from "./firebase";
+import { onAuthStateChanged } from "firebase/auth";
 function App() {
-  const { currentUser } = useContext(AuthContext)||({});
-  console.log(currentUser);
+  
+
+  const { currentUser,setCurrentUser} = useContext(AuthContext);
+  console.log('courrent user in app.js',currentUser);
+  
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   const ProtectedRoute = ({ children }) => {
     if (!currentUser) {
       return <Navigate to="/login" />;
     }
+
+  return children;
+  
   };
   return (
     <div className="App">
