69f28605e9ed46c604e07d035bb189666f07ee69
Essam_Mohamed <99250205+ESSAMMOHAMED1@users.noreply.github.com>
2024-01-27 16:36:45 -0800

    search-user

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,19 +1,56 @@
-import React from 'react'
-import userImg from "../assets/imgs/user.PNG"
+import React, { useContext, useState } from "react";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
 const Search = () => {
-  return (
-    <div className='search'>
-        <div className="searchForm">
-            <input type="text" placeholder='Find user'/>        
-        </div>
-        <div className="userChat">
-            <img src={userImg} alt="" />
-            <div className="userChatInfo">
-                <span>essam</span>
-            </div>
-        </div>
-    </div>
-  )
-}
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [err, setErr] = useState(false);
 
-export default Search
+  const { currentUser } = useContext(AuthContext);
+
+  const handleSearch = async () => {
+    const q = query(
+      collection(db, "users"),
+      where("displayName", "==", username)
+    );
+
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+      });
+    } catch (err) {
+      
+      setErr(true);
+    }
+  };
+
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+  };
+  return (
+    <div className="search">
+      <div className="searchForm">
+        <input
+          type="text"
+          placeholder="Find a user"
+          onKeyDown={handleKey}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      {err && <span>User not found!</span>}
+      
+      {user && (
+        <div className="userChat">
+          <img src={user.photoURL} alt="" />
+          <div className="userChatInfo">
+            <span>{user.displayName}</span>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Search;
